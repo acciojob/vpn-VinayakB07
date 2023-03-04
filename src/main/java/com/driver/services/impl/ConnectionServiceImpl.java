85aec1195c9b29b64d907731a82305c9aa336d5c
Vinayak B <vinayakbalachandra07@gmail.com>
2023-03-04 12:46:34 +0530

    first

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -21,14 +21,33 @@ public class ConnectionServiceImpl implements ConnectionService {
 
     @Override
     public User connect(int userId, String countryName) throws Exception{
-
+    User user=userRepository2.findById(userId).get();
+    Country country=new Country();
+    country.setCountryName(CountryName.valueOf(countryName));
+    country.setCode(CountryName.valueOf(countryName).toCode());
+    user.setMaskedIp(country.getCode());
+    user.setConnected(true);
+    userRepository2.save(user);
+    return user;
     }
     @Override
     public User disconnect(int userId) throws Exception {
-
+    User user=new User();
+    user.setConnected(false);
+    user.setMaskedIp(user.getOriginalIp());
+    userRepository2.save(user);
+    return user;
     }
     @Override
     public User communicate(int senderId, int receiverId) throws Exception {
-
+    User sender=userRepository2.findById(senderId).get();
+    User receiver=userRepository2.findById(receiverId).get();
+    Connection connection=new Connection();
+    connection.setUser(receiver);
+    connection.setServiceProvider(receiver.getCountry().getServiceProvider());
+    sender.getCountry().getServiceProvider().getConnectionList().add(connection);
+    sender.getConnectionList().add(connection);
+    userRepository2.save(sender);
+    return sender;
     }
 }
--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.driver.services.impl;
 
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
+import com.driver.model.User;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
@@ -19,11 +23,33 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(String username, String password, String countryName) throws Exception{
+        User user=new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setConnected(false);
+
+        Country country=new Country();
+        country.setCountryName(CountryName.valueOf(countryName));
+        country.setCode(CountryName.valueOf(countryName).toCode());
+
+        country.setUser(user);
+        user.setOriginalIp(country.getCode());
+        user.setMaskedIp(country.getCode());
+        user.setCountry(country);
+        userRepository3.save(user);
+        return user;
 
     }
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
-
+        User user=userRepository3.findById(userId).get();
+        ServiceProvider serviceProvider=serviceProviderRepository3.findById(serviceProviderId).get();
+        serviceProvider.getCountryList().add(user.getCountry());
+        serviceProvider.getUsers().add(user);
+        user.getCountry().setServiceProvider(serviceProvider);
+        user.getServiceProviderList().add(serviceProvider);
+        userRepository3.save(user);
+        return user;
     }
 }
